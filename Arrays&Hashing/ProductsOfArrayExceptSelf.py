e05e037abd2043758a7ab09e35b7dcc6b43de801
Samridhi <samridhikhosla@gmail.com>
2025-06-20 18:07:26 -0600

    brute force

--- Arrays&Hashing/ProductsOfArrayExceptSelf.py
+++ Arrays&Hashing/ProductsOfArrayExceptSelf.py
@@ -1,8 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-     pass
-
-
-def main():
-   pass
-main()
+        a_dict = {}
+        for i in range(len(nums)):
+            a_dict[i] = 1
+            for j in range(len(nums)):
+                if (i != j):
+                    a_dict[i] = a_dict[i] * nums[j]
+        
+        a_list = []
+        for j in a_dict.values():
+            a_list.append(j)
+        
+        return a_list
