8b8fc76736cebef54a47a55f00379a383a0f274b
Samridhi <samridhikhosla@gmail.com>
2025-07-28 19:01:57 -0600

    ans

--- Arrays&Hashing/ValidSudoku.py
+++ Arrays&Hashing/ValidSudoku.py
@@ -1,8 +1,53 @@
 class Solution:
     def isValidSudoku(self, board: list[list[str]]) -> bool:
+
+        column_flag = True
+
+        for i in board: #column checking
+            valid = set()
+            for j in i:
+                if j in valid and j != ".":
+                    column_flag = False
+                else:
+                    valid.add(j)
         
-    
-        pass
+
+        row_flag = True
+            
+        for i in range(len(board)): #row checking
+            valid = set()
+            for j in range(len(board)):
+                row = board[j][i]
+                if row in valid and row != ".":
+                    row_flag = False
+                else:
+                    valid.add(row)
+
+        
+        square_flag = True
+        current_row = 0
+        current_col = 0
+
+        for cycles in range(9): #square checking
+            valid = set()
+
+            for i in range(current_row, current_row + 3):
+                for j in range(current_col, current_col + 3):
+                    val = board[i][j]  
+                    if val != ".":
+                        if val in valid:
+                            square_flag = False
+                        else:
+                            valid.add(val)
+
+            current_col += 3
+
+            if current_col == 9:
+                current_col = 0
+                current_row += 3
+
+        return row_flag and column_flag and square_flag
+
 
 def main():
     x = Solution()
